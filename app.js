ca6b3eafb0b2d8eb6b586a944b2e2af77ba73764
Ribhu Lahiri <ribhu197@gmail.com>
2018-02-01 10:52:45 -0800

    node.js lab first

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-var hello = require('./routes/hello');
+var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
 
@@ -37,7 +37,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
-app.get('/hello/:userName', hello.view);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
